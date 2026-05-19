49fa4948a5c7af3b19457db765b471dbe5395eb7
Rahul Badachi <rahulsbadachi052@gmail.com>
2026-05-19 11:36:40 +0530

    Refactor API routes with versioned route groups

--- internal/routes/auth_routes.go
+++ internal/routes/auth_routes.go
@@ -11,17 +11,17 @@ func SetupRoutes(router *gin.Engine) {
 
 	api := router.Group("/api/v1")
 	{
-		authRoutes1 := api.Group("/auth")
+		authRoutes := api.Group("/auth")
 		{
-			authRoutes1.POST("/send-otp", handler.SendOTP)
-			authRoutes1.POST("/verify-otp", handler.VerifyOTP)
-			authRoutes1.POST("/register", handler.RegisterUser)
-			authRoutes1.POST("/login", handler.LoginUser)
+			authRoutes.POST("/send-otp", handler.SendOTP)
+			authRoutes.POST("/verify-otp", handler.VerifyOTP)
+			authRoutes.POST("/register", handler.RegisterUser)
+			authRoutes.POST("/login", handler.LoginUser)
 		}
 
-		authRoutes2 := api.Group("/task")
+		taskRoutes := api.Group("/task")
 		{
-			fmt.Print(authRoutes2)
+			fmt.Print(taskRoutes)
 		}
 	}
 }
